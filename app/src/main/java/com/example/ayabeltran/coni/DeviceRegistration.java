d66d0852e757366ef144ab37da9e1b74c85e0e20
Aya <ayabeltran14@gmail.com>
2018-10-05 18:22:53 +0800

    user registration, device reg, user profile, layout intent

--- app/src/main/java/com/example/ayabeltran/coni/DeviceRegistration.java
+++ app/src/main/java/com/example/ayabeltran/coni/DeviceRegistration.java
@@ -1,7 +1,10 @@
 package com.example.ayabeltran.coni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DeviceRegistration extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class DeviceRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_registration);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        Button btnSignup = findViewById(R.id.btn_signup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent  toUserRegistration = new Intent(DeviceRegistration.this, UserRegistration.class);
+                startActivity(toUserRegistration);
+
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toProfile = new Intent(DeviceRegistration.this, UserProfile.class);
+                startActivity(toProfile);
+            }
+        });
+
     }
 }
