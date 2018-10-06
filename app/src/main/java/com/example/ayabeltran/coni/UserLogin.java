305dce26a60aff4ba102f23371d7f01f965a64fe
Aya <ayabeltran14@gmail.com>
2018-10-06 09:27:45 +0800

    Layouts and Variables

--- app/src/main/java/com/example/ayabeltran/coni/UserLogin.java
+++ app/src/main/java/com/example/ayabeltran/coni/UserLogin.java
@@ -2,12 +2,38 @@ package com.example.ayabeltran.coni;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserLogin extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText txtusername, txtpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+        txtusername = findViewById(R.id.usernamelogin);
+        txtpassword = findViewById(R.id.passwordlogin);
+
+        //Calling Database Connection Class
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Reading User Input
+                    //code here
+
+
+                //Database Code
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/ayabeltran/coni/UserRegistration.java
+++ app/src/main/java/com/example/ayabeltran/coni/UserRegistration.java
@@ -2,12 +2,29 @@ package com.example.ayabeltran.coni;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class UserRegistration extends AppCompatActivity {
+
+    Button btnregister;
+    EditText txtlastname,
+            txtfirstname,
+            txtinitial,
+            txtbday,
+            txtage;
+    RadioButton rdbMale, rdbFemale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        txtlastname = findViewById(R.id.childlname);
+        txtfirstname = findViewById(R.id.childfname);
+        txtinitial = findViewById(R.id.childmi);
     }
+
+
 }
